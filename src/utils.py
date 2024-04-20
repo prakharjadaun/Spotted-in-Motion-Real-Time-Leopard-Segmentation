5c4015a1cb0ccaf6d803b2a5b55bf95beec3df9a
Prakhar Jadaun <jadaunprakhar2@gmail.com>
2024-04-21 03:05:39 +0530

    functionality to write json file added

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,40 @@ import sys
 from components.cam_source import CamSource
 from logger import logging
 from exception import CustomException
+import json
+
+i=0
+def _write_output_json(res,filename):
+    """
+    Write the json output to the outputs folder
+
+    Args:
+    - res : result set of the predict function of YOLO
+    - filename: filename as per the image
+
+    Returns:
+    None
+    """
+    # Create a dictionary to store the data
+    output_data = {}
+
+    # Add shape of the image to the dictionary
+    output_data["image_shape"] = res.orig_shape
+
+    # Add confidence score to the dictionary
+    output_data["confidence_score"] = res.boxes.conf.item()
+
+    # Add bounding boxes to the dictionary
+    output_data["bounding_boxes"] = res.boxes.xyxy[0].tolist()
+
+    # Add mask values to the dictionary
+    output_data["mask_values"] = res.masks.xy[0].tolist()
+
+    # Write the dictionary to a JSON file
+    filename = filename + ".json"
+    with open(filename, "w") as json_file:
+        json.dump(output_data, json_file, indent=4)
+        logging.info(f"File {filename} wrote successfully")
 
 
 def _display_detected_frames(conf, model, st_frame, image):
@@ -20,12 +54,21 @@ def _display_detected_frames(conf, model, st_frame, image):
     None
     """
     try:
+        global i
         # Resize the image to a standard size
         image = cv2.resize(image, (720, int(720*(9/16))))
 
         res = model.predict(image, conf=conf)
 
         res_plotted = res[0].plot()
+
+        if(len(res[0].boxes.conf)>0):
+            filename = 'outputs/result'+str(i)
+            cv2.imwrite(filename+'.jpg',res_plotted)
+            _write_output_json(res[0],filename)
+            print("LEOPARD SPOTTED".center(80,'-'))
+            
+            i+=1
         st_frame.image(res_plotted,
                     caption='Segmented Feed',
                     channels="BGR",
@@ -57,6 +100,7 @@ def play_webcam(conf, model):
             try:
                 vid_cap = cv2.VideoCapture(source_webcam)
                 st_frame = st.empty()
+                logging.info("Starting the camera feed")
                 while (vid_cap.isOpened()):
                     success, image = vid_cap.read()
                     if success:
@@ -65,6 +109,7 @@ def play_webcam(conf, model):
                                                 st_frame,
                                                 image,
                                                 )
+
                     else:
                         vid_cap.release()
                         break
